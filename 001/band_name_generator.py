af2875844649fcff010648bd2763c85b3ca0c7a4
Michal Batyra <michal_batyra@epam.com>
2024-02-09 15:53:41 +0100

    one and two

---
+++ 001/band_name_generator.py
@@ -0,0 +1,8 @@
+"""
+Band name generator
+"""
+if __name__ == "__main__":
+    print("Welcome to the Band Name Generator.")
+    city_name = input("What's name of the city you grew up in? ")
+    pet_name = input("What's your pet's name? ")
+    print(f"Your band name could be {city_name.capitalize()} {pet_name.capitalize()}")
---
+++ 002/tip_calculator.py
@@ -0,0 +1,13 @@
+"""
+Calculate the amount of cash each person should give including tips.
+"""
+if __name__ == "__main__":
+    print("Welcome to the tip calculator.")
+    bill = float(input("What was the total bill? $"))
+    no_of_people = int(input("How many people to split the bill? "))
+    tip_percentage = int(input("What percentage tip would you like to give 10, 12 or 15? "))/100
+
+    bill_with_tip = bill + bill*tip_percentage
+    bill_per_person = bill_with_tip / no_of_people
+    amount = round(bill_per_person, 2)
+    print(f"Each person should pay: ${amount}")
