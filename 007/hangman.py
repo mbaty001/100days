f13f85529b49ae938fb0ef982b7480fe4ac71cf4
Michal Batyra <michal_batyra@epam.com>
2024-02-17 15:56:08 +0100

    007 enh

--- 007/hangman.py
+++ 007/hangman.py
@@ -1,94 +1,7 @@
 import random
-
-HANGMAN_WORD = """
- _
-| |
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |
-                   |___/
-"""
-
-HANGMANPICS = [
-    "",
-    """
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-""",
-]
-
-WORDS = [
-    "mama",
-    "tata",
-    "apator",
-    "ziolko",
-    "koziolek",
-    "lupin",
-    "poniedzialek",
-    "zelazko",
-    "szafa",
-]
+from words import WORDS
+from logo import HANGMAN_LOGO
+from hangman_pics import HANGMANPICS
 
 
 def print_word(word):
@@ -98,18 +11,21 @@ def print_word(word):
 
 def main():
     """Entrypoint."""
-    print(HANGMAN_WORD)
+    print(HANGMAN_LOGO)
 
-    target_word = random.choice(WORDS)
+    target_word = random.choice(WORDS).lower()
     guessed_letters = [" _ " for _ in target_word]
     failures = 0
+    used_letters = []
 
     print_word("Try to guess the word :)")
     while failures < len(HANGMANPICS) - 1 and " _ " in guessed_letters:
         print_word(guessed_letters)
         guessed_letter = input("Guess a letter: ").lower()
 
-        if guessed_letter in target_word:
+        if guessed_letter in used_letters:
+            print_word("You have already tried this letter.")
+        elif guessed_letter in target_word:
             for index, letter in enumerate(target_word):
                 if guessed_letter == letter:
                     guessed_letters[index] = f" {letter} "
@@ -117,12 +33,15 @@ def main():
             failures += 1
 
         print(HANGMANPICS[failures])
+        used_letters += guessed_letter
+        print_word(f"Your tries: {' '.join(used_letters)}")
 
     if " _ " not in guessed_letters:
         print_word(guessed_letters)
         print("\nYou win!\n")
     elif failures >= len(HANGMANPICS) - 1:
         print("\nYou lost!\n")
+        print_word(f"Word was: {target_word}")
 
 
 if __name__ == "__main__":
